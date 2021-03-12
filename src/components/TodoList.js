89ababb02c2278d14ff9c84ee4a1cd27cd1d391b
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-03-12 01:04:52 +0100

    1

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import TodoForm from './TodoForm'
+import Todo from './Todo'
 
 function TodoList() {
     const [todos, setTodos] = useState([]);
@@ -14,10 +15,31 @@ function TodoList() {
         setTodos(newTodos);
     };
 
+    const removeTodo = (id) => {
+        const removeArr = [...todos].filter(todo => todo.id !== id)
+
+        setTodos(removeArr);
+    }
+
+    const completeTodo = (id) => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+    }
+
     return (
         <div className='todo-app'>
             <h1>Add to your todo list</h1>
             <TodoForm onSubmit={addTodo}/>
+            <Todo 
+                todos={todos}
+                completeTodo={completeTodo}
+                removeTodo={removeTodo}
+            />
         </div>
     )
 }
