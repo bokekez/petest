2d9e22e73ecb886092e724c93b003793e69b3c81
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-03-15 14:16:42 +0100

    1

--- src/components/ListForm.js
+++ src/components/ListForm.js
@@ -1,38 +1,60 @@
 import React, {useEffect, useState} from 'react'
 
 const ListForm = (props) => {
-    const [listItem, setListItem] = useState('');
-    const [list, setList] = useState([]);
+    const [listItem, setListItem] = useState({
+        name: '',
+        id: 0,
+    });
+    const [list, setList] = useState([{
+        name: '',
+        id: 0,
+    }]);
     const [user, setUser] = useState([]);
 
     const handleChange = (e) =>{
-        setListItem(e.target.value);
+        setListItem({name: e.target.value});
         console.log(listItem)
     }
-    // useEffect(() => {setListItem()})
 
     const handleSubmit = (e) =>{
         e.preventDefault();
         // let addList = listItem;
-        if(listItem !== '') {
+        let idCount = listItem.id + 1;
+        
+        if(listItem.name !== '') {
             setList([...list, listItem])
         }
-        setListItem('');
-        // const mapList = list.map(item =>{
-        //     return <li>{item}</li>
-        // })
-        // setUser(mapList)
+        setListItem({
+            name: '',
+        });
+        console.log(idCount);
+        console.log(listItem);
+        // const tempItems = list.map(item => ({
+        //     'name': item.name
+        // }))
+        // setUser(tempItems)
+
+
+    }
+
+    const deleteItem = () => {
+        list = list.filter(item => item.id !== listItem.id)
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-            <input type='text' placeholder='Add to the list' value={listItem}
+            <input type='text' placeholder='Add to the list' value={listItem.name}
                 name='text' onChange={handleChange}
             ></input>
             <button>Add</button>
-            <pre style={{display:'flex', flexDirection:'column', width: '7rem'}}> {list.slice(0).reverse().map((item) => (
-            <ul><li>{item}</li></ul>
+            {/* <pre style={{display:'flex', flexDirection:'column', width: '7rem'}}> {list.slice(0).reverse().map((item) => (
+            <ul><li>{item.name}</li><button onClick={deleteItem}>Delete</button></ul>
+            ))} */}
+            <pre style={{display:'flex', flexDirection:'column', width: '7rem'}}> {
+                list.slice(0).reverse().map((item) => (
+                <ul><li>{item.name}</li><button onClick={deleteItem}
+                >Delete</button></ul>
             ))}
             </pre>
             </form>
